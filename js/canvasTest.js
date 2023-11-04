a11f7ed3d8438d7034f917126e64477c25c3be22
Charles Path <charles.path@ebankit.com>
2023-11-04 16:06:41 +0000

    Adding and dragging squares is working fine!

--- js/canvasTest.js
+++ js/canvasTest.js
@@ -1,33 +1,123 @@
-// Obtém referências para o canvas e o contexto de desenho
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-// Adiciona um evento de clique para adicionar um item no canvas
-canvas.addEventListener('click', (event) => {
-  // Aqui você pode desenhar o item no contexto do canvas
-  // Por exemplo:
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 50, 50);
-});
+const addSquareButton = document.getElementById('addSquareButton');
+const refreshButton = document.getElementById('refreshButton');
 
-// Adiciona um evento de arrastar para mover o item
 let isDragging = false;
-let offsetX, offsetY;
+let selectedSquare = null;
 
-canvas.addEventListener('mousedown', (event) => {
-  isDragging = true;
-  offsetX = event.clientX - canvas.offsetLeft;
-  offsetY = event.clientY - canvas.offsetTop;
-});
+addSquareButton.addEventListener('click', addSquare);
+refreshButton.addEventListener('click', refreshCanvas);
 
-document.addEventListener('mousemove', (event) => {
-  if (isDragging) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(event.clientX - offsetX, event.clientY - offsetY, 50, 50);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let squares = [];
+
+function addSquare() {
+  const newSquare = {
+    x: Math.random() * (canvas.width - 50),
+    y: Math.random() * (canvas.height - 50),
+    width: 50,
+    height: 50,
+    color: getRandomColor(),
+    prevX: 0,
+    prevY: 0
+  };
+
+  for (let i = 0; i < squares.length; i++) {
+    if (checkCollision(newSquare, squares[i])) {
+      newSquare.x = Math.random() * (canvas.width - 50);
+      newSquare.y = Math.random() * (canvas.height - 50);
+      i = -1;
+    }
   }
-});
 
-document.addEventListener('mouseup', () => {
-  isDragging = false;
-});
+  squares.push(newSquare);
+  redrawCanvas();
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function drawSquare(square) {
+  ctx.fillStyle = square.color;
+  ctx.fillRect(square.x, square.y, square.width, square.height);
+}
+
+function isMouseInsideSquare(square, mouseX, mouseY) {
+  return mouseX > square.x && mouseX < square.x + square.width &&
+         mouseY > square.y && mouseY < square.y + square.height;
+}
+
+function checkCollision(square1, square2) {
+  return !(square1.x + square1.width < square2.x ||
+           square1.x > square2.x + square2.width ||
+           square1.y + square1.height < square2.y ||
+           square1.y > square2.y + square2.height);
+}
+
+function addEventListeners() {
+  canvas.addEventListener('mousedown', (event) => {
+    const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+    for (let i = squares.length - 1; i >= 0; i--) {
+      if (isMouseInsideSquare(squares[i], mouseX, mouseY)) {
+        isDragging = true;
+        selectedSquare = squares[i];
+        break;
+      }
+    }
+  });
+
+  document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+      const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+      const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+      selectedSquare.x = Math.max(0, Math.min(canvas.width - selectedSquare.width, mouseX - selectedSquare.width / 2));
+      selectedSquare.y = Math.max(0, Math.min(canvas.height - selectedSquare.height, mouseY - selectedSquare.height / 2));
+
+      for (let j = 0; j < squares.length; j++) {
+        if (selectedSquare !== squares[j] && checkCollision(selectedSquare, squares[j])) {
+          selectedSquare.x = selectedSquare.prevX;
+          selectedSquare.y = selectedSquare.prevY;
+          break;
+        }
+      }
+
+      selectedSquare.prevX = selectedSquare.x;
+      selectedSquare.prevY = selectedSquare.y;
+      redrawCanvas();
+    }
+  });
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false;
+    selectedSquare = null;
+  });
+}
+
+function redrawCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < squares.length; i++) {
+    drawSquare(squares[i]);
+  }
+}
+
+function refreshCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  squares = [];
+  addEventListeners(); // Re-bind event listeners after refreshing
+}
+
+redrawCanvas();
